6f8ef6a5617f3dd3d0daca690a0dc16444ae4366
achapur <achapur@palaceresorts.com>
2022-08-01 12:58:59 -0500

    2 commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,49 @@ import './App.css';
 import ProductsPage from './components/ProductsPage';
 import axios from 'axios';
 import { useState,useEffect } from "react";
+import data from './data.json'
+import ProductTable from './components/ProductsPage/ProductTable/ProductTable';
+
 
 function App() {
   const [data, setData] = useState({});
   const [listCharacters, setListCharacters] = useState({});
   const [inStock, setInStock] = useState({});
   const handleSearch = (event) => setFiltro(event.target.value);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  const start = () => {
+    setLoading(true); // ajax request after empty completing
+
+    setTimeout(() => {
+      setSelectedRowKeys([]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+  const hasSelected = selectedRowKeys.length > 0;
+
+  useEffect(() => {
+    axios.get((data)=>{
+      .then((result) => {
+        <ProductTable/>
+      })
+      .catch((err) => {
+        console.log('error',err)
+      });
+    })
+      });
+  }, []);
   return (
     <div className="App">
       <ProductsPage />
---
+++ src/components/ProductsPage/ProductTable/ProductTable.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Button, Table } from 'antd';
+
+
+const ProductTable = () => {
+
+    const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+  },
+
+];
+const data = [];
+  return (
+    <div>
+      <div
+        style={{
+          marginBottom: 16,
+        }}
+      >
+        <Button type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
+          Reload
+        </Button>
+        <span
+          style={{
+            marginLeft: 8,
+          }}
+        >
+          {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+        </span>
+      </div>
+      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
+    </div>
+  )
+}
+
+export default ProductTable

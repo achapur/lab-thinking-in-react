a57d1bb14efd556534d09f922fa6d5c8dc11963c
achapur <achapur@palaceresorts.com>
2022-08-01 11:51:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// App.js
+
 import './App.css';
+import ProductsPage from './components/ProductsPage';
+import axios from 'axios';
+import { useState,useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  const [listCharacters, setListCharacters] = useState({});
+  const [inStock, setInStock] = useState({});
+  const handleSearch = (event) => setFiltro(event.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductsPage />
     </div>
   );
 }
---
+++ src/components/ProductsPage/ProductTable/ProductRow/ProductRow.jsx
@@ -0,0 +1,17 @@
+import {name, price} = props
+
+import React from 'react'
+import { Row } from 'react-bootstrap'
+
+const ProductRow = (props) => {
+    const {name,price}=props
+  return (
+    <Row>
+        <Col>
+            
+        </Col>
+    </Row>
+  )
+}
+
+export default ProductRow
